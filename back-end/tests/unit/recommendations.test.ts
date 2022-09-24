1d0b57cee9fcb13def786c1bee719690a5188fa2
andrektozzi <andrektozzi@gmail.com>
2022-09-24 17:04:09 -0300

    test: add unit tests for recommendations id upvote

--- back-end/tests/unit/recommendations.test.ts
+++ back-end/tests/unit/recommendations.test.ts
@@ -1,7 +1,9 @@
 import { jest } from "@jest/globals";
+import { rejects } from "assert";
 import { recommendationRepository } from "../../src/repositories/recommendationRepository";
 import { recommendationService } from "../../src/services/recommendationsService";
 import musicDataFactory from "./factories/recommendationDataFactory";
+import musicListFactory from "./factories/recommendationListFactory";
 
 describe("Test POST /recommendations", () => {
     it("Deve retornar status 200 se postar a recomendação corretamente", async () => {
@@ -28,4 +30,34 @@ describe("Test POST /recommendations", () => {
           type: "conflict",
         });
     });
+});
+
+describe("Test POST /recommendations/:id/upvote", () => {
+  it("Deve retornar status 200 se votar na recomendação corretamente", async () => {
+    const music = await musicDataFactory();
+    const id = 1;
+    
+    jest.spyOn(recommendationRepository, "find").mockImplementationOnce((): any => {
+      return { id, name: music.name, youtubeLink: music.youtubeLink };
+    });
+    
+    jest.spyOn(recommendationRepository, "updateScore").mockImplementationOnce((): any => {});
+    
+    await recommendationService.upvote(id);
+    
+    expect(recommendationRepository.updateScore).toBeCalled();
+  });
+
+  it("Should return 404 if voting for a recommendation that doesn't exist", async () => {
+    await musicDataFactory();
+    const id = 1;
+    
+    jest.spyOn(recommendationRepository, "find").mockImplementationOnce((): any => {});
+    
+    const result = recommendationService.upvote(id);
+    expect(result).rejects.toEqual({
+      message: "",
+      type: "not_found",
+    });
+  });
 });
