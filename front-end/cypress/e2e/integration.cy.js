1e391e5b8c4ff8cfbc00984a4f317e6a7d8f4ca1
andrektozzi <andrektozzi@gmail.com>
2022-09-26 11:33:44 -0300

    test: add e2e tests for downvote recommendations

--- front-end/cypress/e2e/integration.cy.js
+++ front-end/cypress/e2e/integration.cy.js
@@ -17,3 +17,21 @@ describe("Upvote recommendation", () => {
     cy.upvoteRecommendation(recommendation);
   });
 });
+
+describe("Downvote recommendation", () => {
+  it("Testa se vota na recomendação com score maior que -5 corretamente", () => {
+    const recommendation = createRecommendationDataFactory();
+
+    cy.downvoteRecommendation(recommendation, "greater").then((el) => {
+      cy.get('[data-test-id="score"]').should("contain.text", `${el + 1}`);
+    });
+  });
+
+  it("Testa se vota na recomendação com score maior que -5 corretamente", () => {
+    const recommendation = createRecommendationDataFactory();
+
+    cy.downvoteRecommendation(recommendation, "less").then(() => {
+      cy.contains('[data-test-id="score"]').should("not.exist");
+    });
+  });
+});
