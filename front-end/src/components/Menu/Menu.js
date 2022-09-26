2482572edfe5b0fbec10f15e13623ec7beeccf31
andrektozzi <andrektozzi@gmail.com>
2022-09-26 11:39:34 -0300

    test: add cypress commands for view recommendations

--- front-end/src/components/Menu/Menu.js
+++ front-end/src/components/Menu/Menu.js
@@ -15,17 +15,17 @@ export default function Menu() {
         Home
       </MenuItem>
 
-      <MenuItem onClick={() => navigate("/top")}>
+      <MenuItem data-test-id="top" onClick={() => navigate("/top")}>
         <HiTrendingUp />
         Top
       </MenuItem>
 
-      <MenuItem onClick={() => navigate("/random")}>
+      <MenuItem data-test-id="random" onClick={() => navigate("/random")}>
         <FiShuffle />
         Random
       </MenuItem>
     </StyledMenu>
-  )
+  );
 }
 
 const StyledMenu = styled.div`
