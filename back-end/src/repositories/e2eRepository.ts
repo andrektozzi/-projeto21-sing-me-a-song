f96da081498690cf3d0168dc457df15da6fbbe6f
andrektozzi <andrektozzi@gmail.com>
2022-09-26 08:47:28 -0300

    test: add e2e tests for upvote and downvote recommendations

--- back-end/src/repositories/e2eRepository.ts
+++ back-end/src/repositories/e2eRepository.ts
@@ -3,3 +3,18 @@ import { prisma } from "../database.js";
 export async function truncate() {
   await prisma.$executeRaw`TRUNCATE TABLE recommendations`;
 }
+
+export function findByName(name: string) {
+  return prisma.recommendation.findUnique({
+    where: { name },
+  });
+}
+
+export function updateScore(name: string, score: number) {
+  return prisma.recommendation.update({
+    where: { name },
+    data: {
+      score,
+    },
+  });
+}
--- back-end/src/services/e2eService.ts
+++ back-end/src/services/e2eService.ts
@@ -1,5 +1,13 @@
-import * as e2eRepository from '../repositories/e2eRepository.js';
+import * as e2eRepository from "../repositories/e2eRepository.js";
 
 export async function truncate() {
   await e2eRepository.truncate();
+}
+
+export async function getByName(name: string) {
+  return await e2eRepository.findByName(name);}
+
+
+export async function updateScore(name: string, score: number) {
+  return await e2eRepository.updateScore(name, score)
 }
